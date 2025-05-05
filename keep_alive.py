e79bba21edaeff9b7947702a068252d3c0cbc52c
Kaku0201 <kakurim0201@gmail.com>
2025-05-05 17:39:06 +0900

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,17 +1,14 @@
 from flask import Flask
 from threading import Thread
 
-app = Flask('')
-
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "I'm alive!"
-
+    return "봇이 작동 중입니다."  # UptimeRobot이 체크할 응답
 
 def run():
     app.run(host='0.0.0.0', port=8080)
-
 
 def keep_alive():
     t = Thread(target=run)
