f264251a12acdcbbb180ec87b7378b3665bcd70b
Kaku0201 <kakurim0201@gmail.com>
2025-05-17 08:05:32 +0900

    Update test.py

--- cogs/test.py
+++ cogs/test.py
@@ -1,10 +1,1 @@
-print(f"[LOAD] test.py ({__file__})")
-
-from discord.ext import commands
 raise Exception("🐣🐣🐣🐣🐣🐣🐣🐣🐣🐣🐣 test.py 강제 에러 🐣🐣🐣🐣🐣🐣🐣🐣🐣🐣🐣")
-class Test(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-
-async def setup(bot):
-    await bot.add_cog(Test(bot))
